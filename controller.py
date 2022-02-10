25a95967bcc167b8e928f1f67c485dde9713fbad
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2022-02-10 22:18:20 +0300

    control

--- controller.py
+++ controller.py
@@ -3,8 +3,8 @@ import view
 
 def button_click():
     value_a=view.get_vlaue()
-    value_d=view.get_value()
+    value_d=view.get_value() # это ввод знака + - * /
     value_b=view.get_vlaue()
     model.init(value_a, value_d, value_b)
-    result=model.do_it()
+    result=model.calculate()
     view.view_data(result, "result")
