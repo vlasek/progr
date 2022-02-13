660dce44bbd48c0d21227fb3c0f9a42b1b37cefe
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2022-02-13 18:35:44 +0300

    изменил порядок выбора режима калькулятора

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import controller as c
 import print_result
 
 
-mode1 = int(input('Введите режим работы калькулятора: \n1 - рационалные числа, 0 - комплексные: \n'))
+mode1 = int(input('Введите режим работы калькулятора: \n0 - рационалные числа, 1 - комплексные: \n'))
 if mode1 == 1:
-    print_result.print_about(c.button_click())
-else:
     print_result.print_about(c.button_click2())
+else:
+    print_result.print_about(c.button_click())
 
